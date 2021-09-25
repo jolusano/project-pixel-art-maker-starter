ac81f293ca0d00b424c32ade0a4795b1e95e6088
Jose Sanchez <jolusano5@gmail.com>
2021-09-25 02:57:29 -0400

    Define makeGrid function.

--- designs.js
+++ designs.js
@@ -2,9 +2,28 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
+    // Get size of grid to be drawn
+    const height = document.getElementById('inputHeight').valueAsNumber;
+    const width = document.getElementById('inputWidth').valueAsNumber;
 
-// Your code goes here!
+    // remove grid if shown
+    document.body.removeChild(document.getElementsByTagName("table")[0]);
 
+    // Create grid
+    const grid = document.createElement('table');
+    document.body.appendChild(grid);
+    for (let n = 0; n < height; n++) {
+        const row = document.createElement('tr')
+        grid.appendChild(row);
+        for (let m = 0; m < width; m++) {
+            const cell = document.createElement('td')
+            row.appendChild(cell);
+        }
+    }
+    grid.addEventListener('click', function(e) {
+        const selectedColor = document.getElementById("colorPicker").value;
+        if (e.target.nodeName === 'TD')
+            e.target.style.backgroundColor = selectedColor;
+    });
 }
